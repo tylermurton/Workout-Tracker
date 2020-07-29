dfac0ae97eb49de1c654446dea13bbb40498e423
Tyler Murton <tylermurton@gmail.com>
2020-07-29 17:33:41 -0400

    initial commit

---
+++ controller/routes/routes.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const path = require("path");
+
+var routes = express.Router();
+
+const db = require("../../models");
+const Workout = require('../../models/workout');
+
+
+routes.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '../../../public/index.html'));
+  });
+  
+routes.get('/exercise', (req, res) => {
+  res.sendFile(path.join(__dirname + '../../../public/exercise.html'));
+});
+  
+routes.get('/stats', (req, res) => {
+  res.sendFile(path.join(__dirname + '../../../public/stats.html'));
+});
+
+//////////////////////////////
+
+routes.get('/api/workouts', async (req, res) => {
+    Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  
+
+  routes.post('/api/workouts', (req, res) => {
+    Workout.create({
+    }).then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  
+  routes.put('/api/workouts/:id', async (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+  
+    Workout.findById(id)
+      .then(dbWorkout => {
+        dbWorkout.exercises.push(data);
+        return dbWorkout;
+      })
+      .then(dbWorkout => {
+        Workout.findOneAndUpdate(
+          { _id: id },
+          { exercises: dbWorkout.exercises },
+          { new: true }
+        )
+          .then(dbWorkout => {
+            res.json(dbWorkout);
+          })
+          .catch(err => {
+            res.json(err);
+          });
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  
+  routes.get('/api/workouts/range', async (req, res) => {
+    Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
+
+module.exports = routes;
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const routes = require("./controller/routes/routes");
+
+let mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/workouts', 
+ {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log('App now listening at localhost:' + PORT);
+});
